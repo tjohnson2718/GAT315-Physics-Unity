b7fca0704fd26bb10ec9141de25ec25497dcf5f0
tjohnson2718 <tjohnson@student.neumont.edu>
2024-03-27 15:53:21 -0600

    Started Vehicle Physics
    Created a base for the car, and a controller script

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Wheel
+    {
+        public WheelCollider collider;
+    }
+
+    [System.Serializable]
+    public struct Axle
+    {
+        public Wheel leftWheel;
+        public Wheel rightWheel;
+        public bool isMotor;
+        public bool isSteering;
+    }
+
+    [SerializeField] Axle[] axles;
+    [SerializeField] float maxMotorTorque;
+    [SerializeField] float maxSteeringAngle;
+
+    public void FixedUpdate()
+    {
+        float motor = Input.GetAxis("Vertical") * maxMotorTorque;
+        float steering = Input.GetAxis("Horizontal") * maxSteeringAngle;
+
+        foreach (var axle in axles)
+        {
+            if (axle.isSteering)
+            {
+                axle.leftWheel.collider.steerAngle = steering;
+                axle.rightWheel.collider.steerAngle = steering;
+            }
+
+            if (axle.isMotor)
+            {
+                axle.leftWheel.collider.motorTorque = motor;
+                axle.rightWheel.collider.motorTorque = motor;
+            }
+        }
+    }
+}
