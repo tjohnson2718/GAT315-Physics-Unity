ec65052f9769c43438b3fd5e495ff33a92ade171
tjohnson2718 <tjohnson@student.neumont.edu>
2024-04-04 13:33:14 -0600

    Block Buster Continued
    Added some assests, started building the  scene

--- Assets/Scripts/BlockBuster/HorizontalAim.cs
+++ Assets/Scripts/BlockBuster/HorizontalAim.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class HorizontalAim : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float sensitivity = 3;
+
+    Vector3 rotation = Vector3.zero;
+    Vector2 prevAxis = Vector3.zero;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 axis = Vector3.zero;
+
+        axis.x = -Input.GetAxis("Mouse Y") - prevAxis.x;
+
+        rotation.x += axis.x * sensitivity;
+
+        rotation.x = Mathf.Clamp(rotation.x, -50, 50);
+
+        //Quaternion q_yaw = Quaternion.AngleAxis(rotation.y * sensitivity, Vector3.up);
+        Quaternion q_pitch = Quaternion.AngleAxis(rotation.x * sensitivity, Vector3.right);
+
+        transform.localRotation = (q_pitch);
     }
 }
