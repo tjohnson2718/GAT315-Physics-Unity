157b0807d5a23263307afc165470751a20938bae
tjohnson2718 <tjohnson@student.neumont.edu>
2024-04-05 12:39:17 -0600

    Getting near the end of Block Buster
    Added new assets, and built out the world

---
+++ Assets/Scripts/BlockBuster/Building.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+    [SerializeField] GameObject destroyPrefab;
+    [SerializeField] AudioSource sound;
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Vector3 spawnPos = transform.position;
+            spawnPos.y += 5;
+            Instantiate(destroyPrefab, spawnPos, Quaternion.identity);
+            sound.Play();
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ammo"))
+        {
+            health = health - 25;
+        }
+    }
+}
